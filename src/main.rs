1c3d52b8b9eab4567d8a773ee30b6f2128396754
axolotlmaid <hello@axolotlmaid.com>
2024-08-23 23:33:58 +0100

    Add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+use dotenv::dotenv;
 use tokio::net::TcpListener;
 
 mod routes;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     let app = routes::app().await;
 
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    let local_addr = listener.local_addr().unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let port = std::env::var("PORT").expect("PORT must be set.");
+    let listener = TcpListener::bind("0.0.0.0:".to_owned() + port.as_str())
+        .await
+        .unwrap();
 
-    println!("Listening on {}", local_addr);
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    tracing::info!("listening on http://localhost:{}", port);
+
+    axum::serve(listener, app).await.unwrap();
 }
