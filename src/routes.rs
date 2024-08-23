af4879825702961e19216e0fd90028ac15656f0e
axolotlmaid <hello@axolotlmaid.com>
2024-08-23 23:34:13 +0100

    Add routes and favicon

--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,27 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde_json::{json, Value};
+use tower_http::{
+    services::ServeFile,
+    trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
+};
+use tracing::Level;
+
+mod preview;
+mod raw;
 
 pub async fn app() -> Router {
     Router::new()
         .route("/", get(root))
+        .route_service("/favicon.ico", ServeFile::new("favicon.ico"))
+        .route("/:filename/", get(preview::route))
+        .route("/:filename/raw", get(raw::route))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
+        )
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn root() -> Json<Value> {
+    Json(json!({ "message": "hello world." }))
 }
