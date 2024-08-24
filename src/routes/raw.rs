bf8dbbfc7489e53d9048143a0b0b1165b4388449
axolotlmaid <hello@axolotlmaid.com>
2024-08-24 23:39:08 +0100

    add content type to `raw.rs`

--- src/routes/raw.rs
+++ src/routes/raw.rs
@@ -1,14 +1,29 @@
-use axum::{body::Body, extract::Path, http::StatusCode, response::IntoResponse};
+use std::path::PathBuf;
+
+use axum::{
+    body::Body,
+    extract::Path,
+    http::{header, StatusCode},
+    response::IntoResponse,
+};
 use tokio::fs::File;
 use tokio_util::io::ReaderStream;
 
 pub async fn route(Path(filename): Path<String>) -> impl IntoResponse {
-    let file_path = format!("uploads/{}", filename);
+    let file_path = PathBuf::from("uploads").join(&filename);
 
-    let file = match File::open(file_path).await {
+    let file = match File::open(&file_path).await {
         Ok(file) => file,
-        Err(_) => return Err((StatusCode::NOT_FOUND, "file not found.")),
+        Err(_) => return Err((StatusCode::NOT_FOUND, "File not found.")),
     };
+    let content_type = match mime_guess::from_path(&file_path).first_raw() {
+        Some(mime) => mime,
+        None => return Err((StatusCode::BAD_REQUEST, "MIME type could not be determined")),
+    };
+
     let stream = ReaderStream::new(file);
-    Ok(Body::from_stream(stream))
+    let body = Body::from_stream(stream);
+    let headers = [(header::CONTENT_TYPE, content_type)];
+
+    Ok((headers, body))
 }
