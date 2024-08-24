560a46fd26b68aa63991cd20c5a9f300ec6ead5b
axolotlmaid <hello@axolotlmaid.com>
2024-08-24 23:38:54 +0100

    add size and cleanup to `preview.rs`

--- src/routes/preview.rs
+++ src/routes/preview.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, os::unix::fs::MetadataExt, path::PathBuf};
 
 use askama::Template;
 use axum::{extract::Path, http::StatusCode, response::Html};
@@ -9,29 +9,41 @@ use chrono::{DateTime, Local};
 struct PreviewTemplate<'a> {
     file: &'a str,
     file_modified: &'a str,
+    file_size: &'a str,
     page_title: &'a str,
 }
 
 pub async fn route(Path(filename): Path<String>) -> Result<Html<String>, StatusCode> {
-    let page_title = match std::env::var("PAGE_TITLE") {
-        Ok(page_title) => page_title,
-        Err(_) => "files".to_string()
-    };
+    let page_title = std::env::var("PAGE_TITLE").unwrap_or_else(|_| "files".to_string());
 
-    let metadata = match fs::metadata(format!("uploads/{}", filename)) {
-        Ok(metadata) => metadata,
-        Err(_) => return Err(StatusCode::NOT_FOUND),
-    };
-    let modified_time = match metadata.modified() {
-        Ok(time) => DateTime::<Local>::from(time).format("%Y-%m-%d at %H:%M:%S").to_string(),
-        Err(_) => "unknown".to_string(),
-    };
+    let file_path = PathBuf::from("uploads").join(&filename);
+    let metadata = fs::metadata(file_path).map_err(|_| StatusCode::NOT_FOUND)?;
+
+    let modified_time = metadata
+        .modified()
+        .map(|time| {
+            DateTime::<Local>::from(time)
+                .format("%Y-%m-%d at %H:%M:%S")
+                .to_string()
+        })
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    const SIZES: [&str; 5] = ["B", "KB", "MB", "GB", "TB"];
+
+    let size = metadata.size();
+    let size_index = (size as f64).log(1000.0).floor() as usize;
+    let size_divided = size as f64 / 1000_f64.powi(size_index as i32);
+    let size_formatted = format!("{:.1} {}", size_divided, SIZES[size_index]);
 
     let template = PreviewTemplate {
-        file: filename.as_str(),
-        file_modified: modified_time.as_str(),
-        page_title: page_title.as_str(),
+        file: &filename,
+        file_modified: &modified_time,
+        file_size: &size_formatted,
+        page_title: &page_title,
     };
-    let rendered = template.render().unwrap();
-    Ok(Html(rendered))
+
+    template
+        .render()
+        .map(Html)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
