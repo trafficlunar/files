b128d629ef5be8308f01bf71ee3f5da7e6451a95
axolotlmaid <hello@axolotlmaid.com>
2024-08-25 22:26:17 +0100

    add redirect

--- src/routes.rs
+++ src/routes.rs
@@ -1,29 +1,33 @@
-use axum::{routing::get, Json, Router};
-use serde_json::{json, Value};
+use axum::{
+    response::Redirect,
+    routing::{delete, get, post},
+    Router,
+};
 use tower_http::{
     services::ServeFile,
     trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
 };
 use tracing::Level;
 
-mod upload;
+mod delete;
 mod preview;
 mod raw;
+mod upload;
 
 pub async fn app() -> Router {
     Router::new()
-        .route("/", get(root))
+        .route(
+            "/",
+            get(|| async { Redirect::permanent("https://github.com/axolotlmaid/files/") }),
+        )
         .route_service("/favicon.ico", ServeFile::new("favicon.ico"))
-        .route("/upload", get(upload::route))
-        .route("/:filename", get(preview::route))
-        .route("/:filename/raw", get(raw::route))
+        .route("/upload", post(upload::handler))
+        .route("/delete", delete(delete::handler))
+        .route("/:filename", get(preview::handler))
+        .route("/:filename/raw", get(raw::handler))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
                 .on_response(DefaultOnResponse::new().level(Level::INFO)),
         )
-}
-
-async fn root() -> Json<Value> {
-    Json(json!({ "message": "hello world." }))
 }
