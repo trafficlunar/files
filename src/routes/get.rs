1d4d55dbe20219de3ebd2785f681ec099f5af127
axolotlmaid <hello@axolotlmaid.com>
2024-08-28 22:34:09 +0100

    Add `/info` route

--- src/routes/get.rs
+++
@@ -1,2 +0,0 @@
-// todo: create route "/:filename/info"
-// returns { "success": true, "file": "blah", "mimetype": "image/png", "modified": 123405102, "size": 204195219 }
---
+++ src/routes/info.rs
@@ -0,0 +1,34 @@
+use std::{fs, os::unix::fs::MetadataExt, path::PathBuf};
+
+use axum::{extract::Path, http::StatusCode, Json};
+use chrono::{DateTime, Local};
+use serde_json::{json, Value};
+
+pub async fn handler(
+    Path(filename): Path<String>,
+) -> Result<Json<Value>, (StatusCode, Json<Value>)> {
+    let file_path = PathBuf::from("uploads").join(&filename);
+    let metadata = fs::metadata(file_path).map_err(|_| {
+        (
+            StatusCode::NOT_FOUND,
+            Json(json!({ "success": false, "error": "File not found" })),
+        )
+    })?;
+
+    let modified_time = metadata
+        .modified()
+        .map(|time| DateTime::<Local>::from(time).timestamp())
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({ "success": false, "error": "Internal server error while getting the modified time" })),
+            )
+        })?;
+
+    Ok(Json(json!({
+        "success": true,
+        "file": filename,
+        "modified": modified_time,
+        "size": metadata.size()
+    })))
+}
