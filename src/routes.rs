fce9313f38072cc931360461ba8fe11d1eeb5442
axolotlmaid <hello@axolotlmaid.com>
2024-08-28 22:34:32 +0100

    Add 404 and `/info` route

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,10 @@
+use std::time::Duration;
+
 use axum::{
-    extract::DefaultBodyLimit,
-    response::Redirect,
-    routing::{delete, get, post},
-    Router,
+    extract::DefaultBodyLimit, http::StatusCode, response::{IntoResponse, Redirect}, routing::{delete, get, post}, Json, Router
 };
+use serde_json::json;
+use tower::{buffer::BufferLayer, limit::RateLimitLayer};
 use tower_http::{
     services::ServeFile,
     trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
@@ -14,10 +15,11 @@ use tracing::Level;
 #[path = "password.rs"]
 mod password;
 
+mod upload;
 mod delete;
 mod preview;
 mod raw;
-mod upload;
+mod info;
 
 pub async fn app() -> Router {
     // let password = std::env::var("GENERATE_PASSWORD")
@@ -40,10 +42,18 @@ pub async fn app() -> Router {
         .route_service("/favicon.ico", ServeFile::new("favicon.ico"))
         .route("/:filename", get(preview::handler))
         .route("/:filename/raw", get(raw::handler))
+        .route("/:filename/info", get(info::handler))
+        .fallback(handler_404)
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
                 .on_response(DefaultOnResponse::new().level(Level::INFO)),
         )
         .layer(DefaultBodyLimit::max(1024 * 1024 * 1024)) // 1 GB
+        // .layer(BufferLayer::new(1024))        
+        // .layer(RateLimitLayer::new(5, Duration::from_secs(1)))
+}
+
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, Json(json!({ "success": false, "error": "404 - not found"})))
 }
