2bce28a599a71d9bf6875cd93030966405cc70c3
axolotlmaid <hello@axolotlmaid.com>
2024-08-31 21:01:25 +0100

    Add text on startup and create uploads folder on startup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use axum::{extract::Request, ServiceExt};
 use dotenv::dotenv;
 use tokio::net::TcpListener;
@@ -39,12 +41,27 @@ async fn start_metrics_server() {
 
 #[tokio::main]
 async fn main() {
+    let startup_text = r#"
+   __ _ _
+  / _(_) |
+ | |_ _| | ___  ___
+ |  _| | |/ _ \/ __|
+ | | | | |  __/\__ \
+ |_| |_|_|\___||___/
+
+───────────────────────────────────────────────────────────
+github: https://github.com/axolotlmaid/files
+───────────────────────────────────────────────────────────"#;
+    println!("{}", startup_text);
+
     dotenv().ok();
 
     tracing_subscriber::fmt()
         .with_target(false)
         .compact()
         .init();
+
+    fs::create_dir_all("uploads/").unwrap();
 
     let metrics_enabled = std::env::var("METRICS_ENABLED").unwrap_or_else(|_| "false".to_string()) == "true";
 
