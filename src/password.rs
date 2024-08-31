374baabc5ddbef90e9cb86afae3ffe259b6e2ac1
axolotlmaid <hello@axolotlmaid.com>
2024-08-31 14:22:04 +0100

    improve password.rs

--- src/password.rs
+++ src/password.rs
@@ -1,33 +1,31 @@
 use rand::{thread_rng, Rng};
 
-pub fn get_password() -> String {
-    std::env::var("GENERATE_PASSWORD").map_or_else(
-        |_| "false".to_string(),
-        |generate_password| {
-            if generate_password == "true" {
-                const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789)(*&^%$#@!~";
-                let password_length: usize = std::env::var("GENERATE_PASSWORD_LENGTH")
-                    .unwrap_or_else(|_| "16".to_string())
-                    .parse()
-                    .unwrap_or(16);
 
-                let mut rng = thread_rng();
-                let generated_password = (0..password_length)
-                    .map(|_| {
-                        let idx = rng.gen_range(0..CHARSET.len());
-                        CHARSET[idx] as char
-                    })
-                    .collect();
+pub fn get_password() -> String {
+    let generate_password =
+        std::env::var("GENERATE_PASSWORD").unwrap_or_else(|_| "false".to_string()) == "true";
+        
+    if generate_password {
+        let password_length: usize = std::env::var("GENERATE_PASSWORD_LENGTH")
+            .unwrap_or_else(|_| "16".to_string())
+            .parse()
+            .unwrap_or(16);
 
-                tracing::info!("password is `{}`", generated_password);
+        let mut rng = thread_rng();
+        let generated_password = (0..password_length)
+            .map(|_| {
+                let idx = rng.gen_range(0..CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect();
 
-                generated_password
-            } else {
-                std::env::var("PASSWORD")
-                    .expect("PASSWORD must be set when GENERATE_PASSWORD is set to false")
-            }
-        },
-    )
+        tracing::info!("password is `{}`", generated_password);
+        generated_password
+    } else {
+        std::env::var("PASSWORD")
+            .expect("PASSWORD must be set when GENERATE_PASSWORD is set to false")
+    }
 }
