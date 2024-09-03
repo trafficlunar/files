50f818ae43f27b919e6f433a764a6946f8a42fba
axolotlmaid <hello@axolotlmaid.com>
2024-09-03 22:04:23 +0100

    Add ability to choose a generated filename in /api/upload

--- src/routes/api/upload.rs
+++ src/routes/api/upload.rs
@@ -1,20 +1,46 @@
 use std::path::PathBuf;
 
-use axum::{extract::Multipart, http::StatusCode, Json};
+use axum::{
+    extract::{rejection::JsonRejection, Multipart},
+    http::StatusCode,
+    Json,
+};
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
+use serde::Deserialize;
 use serde_json::{json, Value};
 use tokio::fs;
 
-pub async fn handler(mut multipart: Multipart) -> Result<Json<Value>, (StatusCode, Json<Value>)> {
-    let base_url = std::env::var("BASE_URL").expect("BASE_URL must be set.");
-    let generate_filename =
-        std::env::var("GENERATE_FILENAME").unwrap_or_else(|_| "false".to_string());
+#[derive(Deserialize)]
+pub struct UploadSettings {
+    generate_name: bool,
+}
 
-    if let Some(field) = multipart.next_field().await.unwrap() {
+pub async fn handler(
+    result: Result<Json<UploadSettings>, JsonRejection>,
+    mut multipart: Multipart,
+) -> Result<Json<Value>, (StatusCode, Json<Value>)> {
+    let payload = match result {
+        Ok(Json(payload)) => payload,
+        Err(_) => {
+            return Err((
+                StatusCode::BAD_REQUEST,
+                Json(json!({ "success": false, "error": "Invalid request body" })),
+            ))
+        }
+    };
+
+    let base_url = std::env::var("BASE_URL").expect("BASE_URL must be set.");
+    let generate_filename = payload.generate_name
+        || std::env::var("GENERATE_FILENAME")
+            .unwrap_or_else(|_| "false".to_string())
+            .parse::<bool>()
+            .unwrap_or(false);
+
+    if let Some(field) = multipart.next_field().await.unwrap_or(None) {
         let mut name = field.name().unwrap().to_string();
         let data = field.bytes().await.unwrap();
 
-        if generate_filename == "true" {
+        if generate_filename {
             let random_name: String = thread_rng()
                 .sample_iter(&Alphanumeric)
                 .map(char::from)
