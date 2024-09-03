efc82cd258f4faf4dd879c3ea108e8db82c5a4ef
axolotlmaid <hello@axolotlmaid.com>
2024-09-03 22:23:38 +0100

    Revert 50f818ae

--- src/routes/api/upload.rs
+++ src/routes/api/upload.rs
@@ -1,44 +1,22 @@
 use std::path::PathBuf;
 
-use axum::{
-    extract::{rejection::JsonRejection, Multipart},
-    http::StatusCode,
-    Json,
-};
+use axum::{extract::Multipart, http::StatusCode, Json};
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
-use serde::Deserialize;
 use serde_json::{json, Value};
 use tokio::fs;
 
-#[derive(Deserialize)]
-pub struct UploadSettings {
-    generate_name: bool,
-}
-
-pub async fn handler(
-    result: Result<Json<UploadSettings>, JsonRejection>,
-    mut multipart: Multipart,
-) -> Result<Json<Value>, (StatusCode, Json<Value>)> {
-    let payload = match result {
-        Ok(Json(payload)) => payload,
-        Err(_) => {
-            return Err((
-                StatusCode::BAD_REQUEST,
-                Json(json!({ "success": false, "error": "Invalid request body" })),
-            ))
-        }
-    };
-
+pub async fn handler(mut multipart: Multipart) -> Result<Json<Value>, (StatusCode, Json<Value>)> {
     let base_url = std::env::var("BASE_URL").expect("BASE_URL must be set.");
-    let generate_filename = payload.generate_name
-        || std::env::var("GENERATE_FILENAME")
+
+    if let Some(field) = multipart.next_field().await.unwrap_or(None) {
+        let mut name = field.file_name().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+        let file_path = PathBuf::from("uploads").join(&name);
+
+        let generate_filename = std::env::var("GENERATE_FILENAME")
             .unwrap_or_else(|_| "false".to_string())
             .parse::<bool>()
             .unwrap_or(false);
-
-    if let Some(field) = multipart.next_field().await.unwrap_or(None) {
-        let mut name = field.name().unwrap().to_string();
-        let data = field.bytes().await.unwrap();
 
         if generate_filename {
             let random_name: String = thread_rng()
@@ -50,9 +28,14 @@ pub async fn handler(
             name = format!("{}.{}", random_name, name.split(".").last().unwrap());
         }
 
-        let file_path = PathBuf::from("uploads").join(&name);
+        if let Err(err) = fs::write(file_path, &data).await {
+            tracing::error!("Failed to write file: {}", err);
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({ "success": false, "error": "Failed to write file" })),
+            ));
+        };
 
-        fs::write(file_path, &data).await.unwrap();
         tracing::info!("uploaded {} at a size of {} bytes", name, data.len());
 
         return Ok(Json(json!({
