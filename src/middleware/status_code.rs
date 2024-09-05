6f7eddb6cdd6fbeae4bf4c0c3f00ff61e25898d7
axolotlmaid <hello@axolotlmaid.com>
2024-09-05 21:49:44 +0100

    Add comments to notfound.rs and status_code.rs

--- src/middleware/status_code.rs
+++ src/middleware/status_code.rs
@@ -1,6 +1,8 @@
 use axum::{extract::Request, http::StatusCode, middleware::Next, response::Response, Json};
 use serde_json::{json, Value};
 
+// This middleware intercepts every request and changes the response depending on that request's status code
+// There is probably a better way to do this but I couldn't find a documented solution therefore I created this
 pub async fn middleware(
     request: Request,
     next: Next,
--- src/routes/notfound.rs
+++ src/routes/notfound.rs
@@ -1,6 +1,15 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde_json::json;
 
+// Fallback route handler
+
+// Adding this to `/middleware/status_code.rs` will result in other code sending the status code NOT_FOUND
+// to have their response changed. In other words, `preview.rs` tries to send a HTML 404 page with the status
+// code NOT_FOUND and if I add this handler to `status_code.rs` that HTML response will be intercepted and
+// the user will be sent a JSON error instead.
 pub async fn handler() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, Json(json!({ "success": false, "error": "Not found"})))
+    (
+        StatusCode::NOT_FOUND,
+        Json(json!({ "success": false, "error": "Route not found"})),
+    )
 }
